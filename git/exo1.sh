0ddf88321a75fd020b007b891b3809bc1d2e12a7
etienne <laurentlionel20@hotmail.com>
2025-03-19 14:47:38 +0100

    [Update] git script

--- git/exo1.sh
+++ git/exo1.sh
@@ -3,14 +3,14 @@
 # dotnet new gitignore
 # ------ Create Project ------
 
-# ------ Existing Repository ------
-# git add .
-# git commmit -m"[Add] setup console project"
-# ------ Existing Repository ------
-
 # ------ New repository ------
 # git init
 # ------ New repository ------
+
+# ------ Track & Commit File ------
+# git add .
+# git commmit -m"[Add] setup console project"
+# ------ Track & Commit File ------
 
 # ------ Link Remote ------
 # git remode add orign <remote-repository-url>
