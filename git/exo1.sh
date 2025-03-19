af025be8456af5902d16142ab08fca313eddcffb
etienne <laurentlionel20@hotmail.com>
2025-03-19 14:34:59 +0100

    [Add] git script

---
+++ git/exo1.sh
@@ -0,0 +1,20 @@
+# ------ Create Project ------
+# dotnet new console
+# dotnet new gitignore
+# ------ Create Project ------
+
+# ------ Existing Repository ------
+# git add .
+# git commmit -m"[Add] setup console project"
+# ------ Existing Repository ------
+
+# ------ New repository ------
+# git init
+# ------ New repository ------
+
+# ------ Link Remote ------
+# git remode add orign <remote-repository-url>
+# git push -u origin main
+# ------ Link Remote ------
+
+# git push
